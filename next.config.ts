9984416115a164dc6150712e827a01f729ac9c5c
Lakshya Sharma <lakshyasharma1928@gmail.com>
2025-02-18 16:51:32 +0530

    done 16

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-module.exports = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-}
+  // !! WARN !!
+  // Dangerously allow production builds to successfully complete even if
+  // your project has type errors.
+  // !! WARN !!
+  ignoreBuildErrors: true,
+},
+};
+
+export default nextConfig;
